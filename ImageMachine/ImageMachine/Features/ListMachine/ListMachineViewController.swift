61dd4bf1ca8e054fe7f37cb9d885cf7beed0edef
Edwin <osproject78@gmail.com>
2021-12-27 11:29:35 +0700

    Change Initial View Controller

--- ImageMachine/ImageMachine/Features/ListMachine/ListMachineViewController.swift
+++ ImageMachine/ImageMachine/Features/ListMachine/ListMachineViewController.swift
@@ -1,19 +1,15 @@
 //
-//  ViewController.swift
+//  ListMachineViewController.swift
 //  ImageMachine
 //
 //  Created by Edwin Niwarlangga on 27/12/21.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ListMachineViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
