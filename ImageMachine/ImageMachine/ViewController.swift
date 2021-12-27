6b92504cc15c707073699a38b6fd3cecd98952ae
Edwin <osproject78@gmail.com>
2021-12-27 10:17:10 +0700

    Initial Project

---
+++ ImageMachine/ImageMachine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageMachine
+//
+//  Created by Edwin Niwarlangga on 27/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
