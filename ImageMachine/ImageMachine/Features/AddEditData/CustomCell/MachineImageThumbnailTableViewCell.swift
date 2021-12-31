5241f3956cc9fc7580d19282ea7346114dd007e8
Edwin <osproject78@gmail.com>
2021-12-31 21:31:50 +0700

    Update UI and Function Add Edit Data

--- ImageMachine/ImageMachine/Features/AddEditData/CustomCell/MachineImageThumbnailTableViewCell.swift
+++ ImageMachine/ImageMachine/Features/AddEditData/CustomCell/MachineImageThumbnailTableViewCell.swift
@@ -9,19 +9,28 @@ import UIKit
 
 class MachineImageThumbnailTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var machineImageDeleteButtonPressed: UIButton!
-    @IBOutlet weak var machineImageDeleteButton: UIButton!
+    @IBOutlet weak var viewRounded: UIView!
     @IBOutlet weak var machineImageName: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        machineImageDeleteButton.setTitle("", for: .normal)
         // Initialization code
+        
+        viewRounded.backgroundColor = UIColor.white
+        viewRounded.layer.cornerRadius = 15.0
+        viewRounded.layer.shadowColor = UIColor.gray.cgColor
+        viewRounded.layer.shadowOffset = CGSize(width: 1, height: 1)
+        viewRounded.layer.shadowRadius = 1
+        viewRounded.layer.shadowOpacity = 5
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func setImageName(title : String){
+        machineImageName.text = title
     }
     
 }
